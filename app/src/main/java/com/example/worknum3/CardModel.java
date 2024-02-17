287bea4c23c175d89dc93f846cf9db2a0861a993
eliya <eliyacaspi4@gmail.com>
2024-02-17 19:02:14 +0200

    תיקונים של שגיאות

--- app/src/main/java/com/example/worknum3/CardModel.java
+++ app/src/main/java/com/example/worknum3/CardModel.java
@@ -1,13 +1,14 @@
 package com.example.worknum3;
 
 public class CardModel {
-    private int imageResource;
-    private String text;
+     int imageResource;
+    String text;
 
     public CardModel(int imageResource, String text) {
         this.imageResource = imageResource;
         this.text = text;
     }
+
 
     public int getImageResource() {
         return imageResource;
